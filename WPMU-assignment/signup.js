ce005a0f532474805523f4eba6945d3a6f4f8271
Minh Bui <tranduyminh.bui@gmail.com>
2020-06-05 23:25:20 +1000

    finish static layout with formValidator function and mobile responsive

---
+++ WPMU-assignment/signup.js
@@ -0,0 +1,50 @@
+const formSignup = document.getElementById('form-signup');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const emailError = document.getElementById('email-error');
+const passwordError = document.getElementById('password-error');
+
+formSignup.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formValidator();
+  setTimeout(() => {
+    clearError(emailError, email);
+    clearError(passwordError, password);
+  }, 5000);
+});
+
+email.addEventListener('onchange', () => clearError(emailError, email));
+
+const formValidator = () => {
+  if (!name.value || !email.value || !password.value) {
+    console.log('please fill all fields');
+  }
+  if (!emailValidator(email.value)) {
+    displayError(emailError, email, 'Please enter a valid email address');
+  }
+  if (password.value.length < 8) {
+    displayError(
+      passwordError,
+      password,
+      'Password must have 8 characters minimum'
+    );
+  }
+};
+
+const emailValidator = (email) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+};
+
+const clearError = (errorElement, element) => {
+  errorElement.innerText = null;
+  element.nextElementSibling.style.color = '#999';
+  element.style.border = '1px solid #ededed';
+};
+
+const displayError = (errorElement, element, message) => {
+  errorElement.innerText = message;
+  element.nextElementSibling.style.color = 'red';
+  element.style.border = '1px solid red';
+};
